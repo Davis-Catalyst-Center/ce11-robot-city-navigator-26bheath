5d6d29b979950e524f9e1686a62925bd11982ad8
26bheath <greenheath11@gmail.com>
2026-05-14 21:46:06 +0000

    Finished CE11

--- main.cpp
+++ main.cpp
@@ -18,19 +18,41 @@ int main() {
     std::cout << "--- Route: Downtown -> Airport ---\n\n";
 
     /* Test all three algorithms on this route */
+        auto resultDijkstra = city.dijkstraPath(0, 2);
+        printResult("Dijkstra's Algorithm", resultDijkstra);
+    
+        auto resultAStar = city.aStarPath(0, 2);
+        printResult("A* Search Algorithm", resultAStar);
+    
+        auto resultBFS = city.greedyPath(0, 2);
+        printResult("Greedy Search", resultBFS);
 
     std::cout << "\n";
 
     std::cout << "--- Route: Downtown -> Suburb South ---\n\n";
 
     /* Test all three algorithms on this route */
-
+        resultDijkstra = city.dijkstraPath(0, 7);
+        printResult("Dijkstra's Algorithm", resultDijkstra);
+    
+        resultAStar = city.aStarPath(0, 7);
+        printResult("A* Search Algorithm", resultAStar);
+    
+        resultBFS = city.greedyPath(0, 7);
+        printResult("Greedy Search", resultBFS);
     std::cout << "\n";
 
     std::cout << "--- Route: [Your Choice] ---\n\n";
 
     /* Pick your own start/end pair and test all three algorithms */
-
+        resultDijkstra = city.dijkstraPath(3, 2);
+            printResult("Dijkstra's Algorithm", resultDijkstra);
+        
+            resultAStar = city.aStarPath(3, 2);
+            printResult("A* Search Algorithm", resultAStar);
+        
+            resultBFS = city.greedyPath(3, 2);
+            printResult("Greedy Search", resultBFS);
     return 0;
 }
 
